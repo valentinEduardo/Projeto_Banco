814bd59136fb29f94a9cea3d7d941f627cde1336
Vinicius Carvalho <114104425+ViniciusCarvalhoDevy@users.noreply.github.com>
2022-09-22 17:16:16 -0300

    Titular2

---
+++ Titular2.java
@@ -0,0 +1,41 @@
+public class Titular2 extends TitularMaster{
+
+    public Titular2() {
+        super(4596, 222222222,65893);
+        setSaldo2(2000);
+    }
+    public Integer getCodTitular2() {
+        return super.getCodTitular();
+    }
+    //public void setCodTitular() {
+       // super.setCodTitular();
+    //}
+    public Integer getCpfLogin2() {
+        return super.getCpfLogin();
+    }
+    public void setCpfLogin2(Integer novoLogin) {
+      super.setCpfLogin(novoLogin);
+    }
+    public Integer getSenha2() {
+        return super.getSenha();
+    }
+    public void setSenha2(Integer novaSenha) {
+        super.setSenha(novaSenha);
+    }
+    public Integer getSaldo2() {
+        return super.getSaldo();
+    }
+    public void setSaldo2(Integer saldo) {
+        super.setSaldo(saldo);
+    }
+
+    public Integer getQuantidadeSaque2() {
+        return super.getQuantidadeSaque();
+    }
+
+    public void setSaque2(Banco bank,Integer valorSaque,Integer tipoNota[],int quant[]){
+        super.setSaque(bank, valorSaque, tipoNota, quant);
+        
+
+    }
+}
