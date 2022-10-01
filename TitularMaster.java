70a76acdc6ed0c0dba1da65dd80e9c7b006fab45
Valentin Eduardo <101229730+ValentinDevbr@users.noreply.github.com>
2022-10-01 15:10:31 -0300

    Update TitularMaster.java

--- TitularMaster.java
+++ TitularMaster.java
@@ -1,63 +1,66 @@
 abstract class TitularMaster {
-    private Integer codTitular;
-    private  String cpfLogin;
-    private Integer senha;
-    private Integer saldo;
-    private Integer quantidadeSaque;
-    public TitularMaster(Integer cod,String cpfLogin,Integer senha) {
-        this.codTitular = cod;
-        this.cpfLogin = cpfLogin;
-        this.senha = senha;
+  private Integer codTitular;
+  private String cpfLogin;
+  private Integer senha;
+  private Integer saldo;
+  private Integer quantidadeSaque;
+
+  public TitularMaster(Integer cod, String cpfLogin, Integer senha) {
+    this.codTitular = cod;
+    this.cpfLogin = cpfLogin;
+    this.senha = senha;
+  }
+
+  public Integer getCodTitular() {
+    return this.codTitular;
+  }
+
+  public void setCodTitular(int codTitular) {
+    this.codTitular = codTitular;
+  }
+
+  public String getCpfLogin() {
+    return this.cpfLogin;
+  }
+
+  public void setCpfLogin(String cpfLogin) {
+    this.cpfLogin = cpfLogin;
+  }
+
+  public Integer getSenha() {
+    return this.senha;
+  }
+
+  public void setSenha(Integer senha) {
+    this.senha = senha;
+  }
+
+  public Integer getSaldo() {
+    return this.saldo;
+  }
+
+  public void setSaldo(Integer saldo) {
+    this.saldo = saldo;
+  }
+
+  public Integer getQuantidadeSaque() {
+    return quantidadeSaque;
+  }
+
+  public void setSaque(Banco bank, Integer valorSaque) {
+    if (valorSaque % 2 == 0) {
+      bank.setSaldoBancoTotalSaque(valorSaque);
+      System.out.println("\r\nSaque feito con Sucesso!\r\n");
+    } else {
+      System.out.println("Ocorreu um Erro! Valor Solicitado e Impar!");
+      System.out.println("\r\nPor favor, Insira um Valor Par!!");
     }
 
-    
-    public Integer getCodTitular() {
-        return this.codTitular;
-    }
-    public void setCodTitular(int codTitular) {
-        this.codTitular = codTitular;
-    }
-    public String getCpfLogin() {
-        return this.cpfLogin;
-    }
-    public void setCpfLogin(String cpfLogin) {
-        this.cpfLogin = cpfLogin;
-    }
-    public Integer getSenha() {
-        return this.senha;
-    }
-    public void setSenha(Integer senha) {
-        this.senha = senha;
-    }
-    public Integer getSaldo() {
-        return this.saldo;
-    }
-    public void setSaldo(Integer saldo) {
-        this.saldo = saldo;
-    }
+  }
 
-    public Integer getQuantidadeSaque() {
-        return quantidadeSaque;
-    }
+  public void setDepositoIndivi(Integer valorD) {
+    this.saldo += valorD;
+    System.out.println("\r\nValor Depositado Com Sucesso!\r\n");
+  }
 
-   public void setSaque(Banco bank,Integer valorSaque){
-        if(valorSaque%2 == 0 ){
-           System.out.println("\r\nSaldo do Banco Antes:"+bank.getSaldoBancoTotal());
-        bank.setSaldoBancoTotalSaque(valorSaque);
-           System.out.println("\r\nSaldo do Banco:"+bank.getSaldoBancoTotal());
-        System.out.println("\r\nSaque feito con Sucesso!\r\n");
-        }else {
-            System.out.println("Ocorreu um Erro! Valor Solicitado e Impar!");
-            System.out.println("\r\nPor favor, Insira um Valor Par!!");
-        }       
-
-    }
-   
-    public void setDepositoIndivi(Integer valorD) {
-        this.saldo += valorD;
-        System.out.println("\r\nValor Depositado Com Sucesso!\r\n");
-    }
-   
-
-    
 }
